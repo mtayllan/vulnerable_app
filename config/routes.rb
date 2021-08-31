736e6c12e92a1eca6b51ff57636063d9e9b922b1
mtayllan <messiastayllan@gmail.com>
2021-08-31 20:50:20 -0300

    restore excluded route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
       get 'use'
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
 end
